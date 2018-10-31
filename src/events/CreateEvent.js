e974228065de35c78d6884e129db702eefb4edd8
bharu06 <manusanibhargavi14@gmail.com>
2018-10-31 23:19:26 +0530

    added event name

--- src/events/CreateEvent.js
+++ src/events/CreateEvent.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class CreateEvent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      eventName: ""
+    };
+  }
+
+  handleEventChange = (event) => {
+    this.setState({
+      eventName: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="">
         <header className="">
-          
+          <h5> Event Name: </h5>
+          <input type="text" value={this.state.eventName} onChange={this.handleEventChange}/>
         </header>
       </div>
     );
