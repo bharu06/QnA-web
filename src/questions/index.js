d79ab11825c1e22ba5a4ba37121f4cfe4b17a90f
bharu06 <manusanibhargavi14@gmail.com>
2018-10-31 23:06:08 +0530

    added questions page

--- src/questions/index.js
+++ src/questions/index.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import './styles.css';
 
 class Question extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      questions: [{
+        "question": "Is micromanagement really that bad?",
+        "upvotes": 10
+      }, {
+        "question": "Is micromanagement really that bad?",
+        "upvotes": 10
+      },{
+        "question": "Is micromanagement really that bad?",
+        "upvotes": 10
+      }, {
+        "question": "Is micromanagement really that bad?",
+        "upvotes": 10
+      }, {
+        "question": "Is micromanagement really that bad?",
+        "upvotes": 10
+      }, {
+        "question": "Is micromanagement really that bad?",
+        "upvotes": 10
+      }]
+    };
+  }
+
+  renderQuestions = () => {
+    return (
+      <table>
+        {this.renderTableBody()}
+      </table>
+    );
+  }
+
+  renderTableBody = () => {
+    let questions = this.state.questions;
+    let tableBody = questions.map((question) => {
+      return (
+        <tr>
+          <td> Question:</td>
+          <td> {question.question} </td>
+          <td> {question.upvotes} </td>
+        </tr>
+      );
+    });
+    return tableBody;
+  }
+
   render() {
     return (
       <div className="">
-        <header className="App-header">
-          <h1>
-            questions page
-          </h1>
+        <header className="header">
+          {this.renderQuestions()}
         </header>
       </div>
     );
